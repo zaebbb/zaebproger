eb79e197b9b2ddec2e353ea2df0e7ba4534d56fe
Vladimir Zaeb <65035479+zaebbb@users.noreply.github.com>
2021-10-11 23:18:57 +0500

    Add files via upload

---
+++ script/animate_script.js
@@ -0,0 +1,110 @@
+(function(){
+    let canvas = document.createElement('canvas'),
+    ctx = canvas.getContext('2d'),
+    w = canvas.width = innerWidth,
+    h = canvas.height = innerHeight,
+    particles = [],
+    properties = {
+        bg: 'rgba(17,17,19,1)',
+        //partColor: 'rgba(255,40,40,1)',
+        partColor: 'rgba(192,176,207,1)',
+        partRadius: 3,
+        partCount: 100,
+        partMaxVelocity: 2.5,
+        lineLength: 190,
+        partLife: 6
+    };
+    document.querySelector('footer').appendChild(canvas);
+
+    window.onresize = function(){
+        w = canvas.width = innerWidth;
+        h = canvas.height = innerHeight;
+    }
+
+    class Particle{
+        constructor(){
+            this.x = Math.random() * w;
+            this.y = Math.random() * h;
+            this.velocityX = Math.random() * (properties.partMaxVelocity * 2) - properties.partMaxVelocity;
+            this.velocityY = Math.random() * (properties.partMaxVelocity * 2) - properties.partMaxVelocity;
+            this.life = Math.random() * properties.partLife * 60;
+
+        }
+        position(){
+            this.x + this.velocityX > w && this.velocityX > 0 || this.x + this.velocityX < 0 && this.velocityX < 0 ? this.velocityX *= -1 : this.velocityX;
+            this.y + this.velocityY > h && this.velocityY > 0 || this.y + this.velocityY < 0 && this.velocityY < 0 ? this.velocityY *= -1 : this.velocityY;
+            this.x += this.velocityX;
+            this.y += this.velocityY;
+        }
+        reDraw(){
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, properties.partRadius, 0, Math.PI * 2);
+            ctx.closePath();
+            ctx.fillStyle = properties.partColor;
+            ctx.fill();
+        }
+        reCalculateLife(){
+            if (this.life < 1){
+                this.x = Math.random() * w;
+                this.y = Math.random() * h;
+                this.velocityX = Math.random() * (properties.partMaxVelocity * 2) - properties.partMaxVelocity;
+                this.velocityY = Math.random() * (properties.partMaxVelocity * 2) - properties.partMaxVelocity;
+                this.life = Math.random() * properties.partLife * 60;
+            }
+            this.life--;
+        }
+    }
+
+    function reDrawBackground(){
+        ctx.fillStyle = properties.bg;
+        ctx.fillRect(0, 0, w, h);
+    }
+
+    function drawLines(){
+        let x1, y1, x2, y2, length, opacity;
+        for(let i in particles){
+            for(let j in particles){
+                x1 = particles[i].x;
+                y1 = particles[i].y;
+                x2 = particles[j].x;
+                y2 = particles[j].y;
+                length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+                if (length < properties.lineLength){
+                    opacity = 1 - length / properties.lineLength;
+                    ctx.lineWidth = '0.5';
+                    ctx.strokeStyle = 'rgba(95,61,137, '+opacity+')';
+                    // ctx.strokeStyle = 'rgba(255, 40, 40, '+opacity+')';
+                    ctx.beginPath();
+                    ctx.moveTo(x1, y1);
+                    ctx.lineTo(x2, y2);
+                    ctx.closePath();
+                    ctx.stroke();
+                }
+            }
+        }
+    }
+
+    function redrawParticles(){
+        for(let i in particles){
+            particles[i].reCalculateLife();
+            particles[i].position();
+            particles[i].reDraw();
+        }
+    }
+
+    function loop(){
+        reDrawBackground();
+        redrawParticles();
+        drawLines();
+        requestAnimationFrame(loop);
+    }
+
+    function init(){
+        for(let i = 0; i < properties.partCount;i++){
+            particles.push(new Particle);
+        }
+        loop();
+    }
+
+    init();
+}())
---
+++ script/script.js
@@ -0,0 +1,211 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+  document.querySelector('.timeDate').textContent = new Date().getFullYear();
+
+    const swiper = new Swiper('.swiper', {
+      direction: 'horizontal',
+      loop: true,
+      slidesPerView: 10,
+      speed: 1500,
+      autoplay: {
+        delay: 0,
+        disableOnInteraction: false
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        480: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+        720: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        860: {
+          slidesPerView: 6,
+          spaceBetween: 50,
+        },
+        1000: {
+          slidesPerView: 7,
+          spaceBetween: 60,
+        },
+        1300: {
+          slidesPerView: 10,
+          spaceBetween: 70,
+        },
+      }
+    })
+    const swiper_3 = new Swiper('.swiper_clients', {
+      direction: 'horizontal',
+      // loop: true,
+      slidesPerView: 10,
+      speed: 1500,
+      autoplay: {
+        delay: 0,
+        disableOnInteraction: false
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        480: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+        720: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        860: {
+          slidesPerView: 6,
+          spaceBetween: 50,
+        },
+        1000: {
+          slidesPerView: 7,
+          spaceBetween: 60,
+        },
+        1300: {
+          slidesPerView: 10,
+          spaceBetween: 70,
+        },
+      }
+    })
+
+    const swiper_2 = new Swiper('.swiperMain', {
+        loop: true,
+        scrollbar: false,
+        speed: 1500,
+        pagination: {
+          el: '.swiper_pagination',
+        },
+        navigation: {
+          nextEl: '.swiper_button_next',
+          prevEl: '.swiper_button_prev',
+        },
+    })
+
+  setInterval(() => {
+    if(window.pageYOffset > 800){
+        document.querySelector('.sci').classList.add('activeSci');
+    } else {
+      document.querySelector('.sci').classList.remove('activeSci');
+    }
+    if(window.pageYOffset > 200){
+      document.querySelector('.arrowScrolling').classList.add('scroller');
+    } else {
+      document.querySelector('.arrowScrolling').classList.remove('scroller');
+    }
+    
+  }, 200)
+  
+
+  async function databaseSlider(){
+    let api = './../json/slider_db.json';
+    const loadData = await fetch(api);
+    const readData = await loadData.json();
+
+    readData.data.map(el => {
+      console.log(el);
+      document.querySelector('.mainSliderServices').innerHTML += `
+          <div class="swiper-slide mainSlide width_content">
+              <video src="./video/services/${el.url}" autoplay loop muted></video>
+              <div class="content_slider_check">
+                <h1>${el.title}</h1>
+                <p>${el.descr}</p>
+                <p class="priceServices">${el.price}</p>
+              </div>
+          </div>
+      `;
+    })
+  }
+
+  async function databaseSlider_2(){
+    let api = './../json/slider_db_2.json';
+    const loadData = await fetch(api);
+    const readData = await loadData.json();
+
+    readData.data.map(el => {
+      document.querySelector('.slider_technology').innerHTML += `
+          <div class="swiper-slide slideTechnology">
+            <img src="./img/technology/${el.url}">
+          </div>
+      `;
+    })
+  }
+
+  async function databaseSlider_3(){
+    let api = './../json/slider_db_3.json';
+    const loadData = await fetch(api);
+    const readData = await loadData.json();
+
+    readData.data.map(el => {
+      document.querySelector('.swiper_clients_wrapper').innerHTML += `
+          <div class="swiper-slide slide_client">
+            <img src="./img/clients/${el.url}">
+          </div>
+      `;
+    })
+  }
+
+  async function cards(){
+    let api = './../json/cards.json';
+    const loadData = await fetch(api);
+    const readData = await loadData.json();
+
+    readData.data.map(el => {
+      document.querySelector('.cards_products').innerHTML += `
+          <div class="cardProject">
+            <h3 class="title">${el.title}</h3>
+            <div class="card_job">
+                <p>${el.type} </p>
+            </div>
+            <a href="#" class="moreInfo" onclick="return modalSettingSite();">
+                ПОДРОБНЕЕ
+            </a>
+            <img src="./img/cards/${el.image}">
+          </div>
+      `;
+    })
+  }
+
+  setTimeout(() => {
+      databaseSlider();
+      databaseSlider_2();
+      databaseSlider_3();
+      cards();
+  }, 200)
+})
+
+function menuToggle(){
+  const toggle = document.querySelector(".toggle");
+  const section = document.querySelector(".header_section");
+  toggle.classList.toggle("active");
+  section.classList.toggle("active");
+}
+
+function modalSettingSite(){
+
+  document.querySelector('.messageTechEnginering').classList.toggle('active');
+
+  return false;
+}
+
+function scrollingToCheckHeight(){
+  if(window.pageYOffset > 200){
+    window.scrollTo(0, 0)
+  } else {
+    window.scrollTo(0, 900)
+  }
+}
